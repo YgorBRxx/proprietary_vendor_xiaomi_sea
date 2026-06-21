3ec7ce4dd52d8670b17312aa78273932bd846041
YgorBRxx <ygorbrxx@noreply.codeberg.org>
2026-06-21 07:58:44 -0300

    vendor: update LK blob

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ ifeq ($(TARGET_DEVICE),sea)
 
 $(call add-radio-file-sha1-checked,radio/audio_dsp.img,0fb0a76db6ea2225f7ce20be1ae7b2aa254d3551)
 $(call add-radio-file-sha1-checked,radio/gz.img,934e5839876fbafc1f19fb33ecd7e43e34c29669)
-$(call add-radio-file-sha1-checked,radio/lk.img,35e5debc2f05976b0b214b80775a77cd62b0d3c6)
+$(call add-radio-file-sha1-checked,radio/lk.img,e1733de1bb3a31808acb0888c091704fda550e8a)
 $(call add-radio-file-sha1-checked,radio/md1img.img,5c36503869fa7a42d1f1571b2b5b5f9467c0efb9)
 $(call add-radio-file-sha1-checked,radio/pi_img.img,74e5cd3e6f51891683afad4813c350dff9da34be)
 $(call add-radio-file-sha1-checked,radio/preloader_raw.img,e566d1ec5b184bce9c036b6c505addeb8d60fe0b)
